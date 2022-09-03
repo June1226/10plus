fe46e641951518f06ade37141ccd54fc18f1d27d
June1226 <309338743@qq.com>
2022-09-03 19:32:47 +0800

    增加10以内减法

--- 10minus.py
+++ 10minus.py
@@ -7,17 +7,17 @@ import openpyxl as op
 from openpyxl.styles import Font
 
 
-# 生成10以内加法计算公式
-def ten_plus(num):
+# 生成10以内减法计算公式
+def ten_minus(num):
     ten_list = []
 
     while True:
         a = random.randint(1, 10)
-        # 避免出现0+0
         b = random.randint(1, 10)
-        # 控制和为10以内
-        if a + b <= 10:
-            ten_list.append(f"{a} + {b} = ")
+        # 判断数字大小，避免出现负数
+        if a < b:
+            a, b = b, a
+            ten_list.append(f"{a} - {b} = ")
         # 题目数量
         if len(ten_list) == num: break
     return ten_list
@@ -25,7 +25,7 @@ def ten_plus(num):
 
 # 将公式写入excel表
 def write_datas(num):
-    datas = ten_plus(num)
+    datas = ten_minus(num)
 
     # 新建excel表
     wb = op.Workbook()
@@ -37,12 +37,12 @@ def write_datas(num):
         del_datas = datas[i: i + 2]
         ws.cell(row=n + 1, column=1, value=del_datas[0])
         ws.cell(row=n + 1, column=3, value=del_datas[1])
-    wb.save('10plus.xlsx')
+    wb.save('10minus.xlsx')
 
 
 # 设置excel格式
 def set_style(num):
-    wb = op.load_workbook("10plus.xlsx")
+    wb = op.load_workbook("10minus.xlsx")
     ws = wb['Sheet']
 
     # 调整列宽,行高
@@ -66,7 +66,8 @@ def set_style(num):
         for cell in row:
             cell.font = font
 
-    wb.save("10plus.xlsx")
+    wb.save("10minus.xlsx")
+
 
 if __name__ == '__main__':
     num = int(input("请输入打印题目数量（双数）："))
